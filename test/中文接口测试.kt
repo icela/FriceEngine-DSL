7a45ff3efd8d0d60a152390d338b2d1765692a43
fromwheretowhere <fromwheretowhere.service@gmail.com>
2017-12-19 14:32:33 -0800

    复制Test.kt

---
+++ test/中文接口测试.kt
@@ -0,0 +1,60 @@
+import org.frice.dsl.game
+
+fun main(args: Array<String>) {
+	game {
+		bounds(500, 500, 800, 750)
+		showFPS = false
+
+		whenExit {
+			closeWindow()
+		}
+
+		whenUpdate {
+			if (800.elapsed()) {
+				rectangle {
+					x = elapsed / 10.0
+					y = elapsed / 10.0
+					color = PINK
+				}
+			}
+		}
+
+		every(1000) {
+			oval {
+				x = elapsed / 10.0
+				y = elapsed / 10.0
+				color = ORANGE
+			}
+			log("1 second has past.")
+		}
+
+		rectangle {
+			name("rectangle")
+			x = 100.0
+			y = 100.0
+			width = 100.0
+		}
+		oval {
+			x = 0.0
+			y = 85.0
+			accelerate {
+				x = 10.0
+//				y = 10.0
+			}
+			whenColliding("rectangle") {
+				stop()
+				x -= 5
+				accelerate(-2, 10)
+			}
+		}
+		image {
+			file("C:/frice.png")
+			x = 200.0
+			y = 300.0
+			velocity {
+				x = -5.5
+//				y = -5.5
+			}
+		}
+	}
+}
